0ddd52e5e14fef9c331584aaad8edc0fcca4f3fe
olena solontsova <olena.solontsova@sigma.software>
2024-09-27 17:03:56 +0300

    Added PixiJS scenes and some bad HTML5 animations

--- index.js
+++ index.js
@@ -14,18 +14,43 @@ const style = new PIXI.TextStyle({
 	align: "center"
 });
 
+//const blurFilter = new PIXI.filters.BlurFilter();
+
+const loaderScene = new PIXI.Container();
+
       // Create the sprite and add it to the stage
       await PIXI.Assets.load('resources/loader.png');
       let sprite = PIXI.Sprite.from('resources/loader.png');
-      app.stage.addChild(sprite);
+      loaderScene.addChild(sprite);
+	  // TODO: change anchor point
+	  sprite.x = 160;
 	  sprite.y = 100;
 
       // Add a ticker callback to move the sprite back and forth
       let elapsed = 0.0;
       app.ticker.add((ticker) => {
         elapsed += ticker.deltaTime;
-        sprite.x = 170.0 + Math.cos(elapsed/25.0) * 10.0;
+		sprite.width += Math.cos(elapsed/25.0);
+		sprite.height += Math.cos(elapsed/25.0);
       });
+/*
+	await PIXI.Assets.load('resources/Loader_HTML5Canvas.svg');
+	const spriteSvg = PIXI.Assets.load({
+        src: 'resources/Loader_HTML5Canvas.svg',
+        data: {
+            parseAsGraphicsContext: true,
+        },
+    });
+
+    const graphics = new PIXI.Graphics(spriteSvg);
+
+    // line it up as this svg is not centered
+    const bounds = graphics.getLocalBounds();
+    graphics.pivot.set((bounds.x + bounds.width) / 2, (bounds.y + bounds.height) / 2);
+    graphics.position.set(app.screen.width / 2, app.screen.height / 2);
+
+    app.stage.addChild(graphics);
+	*/
 
 // Create a basic button shape using PIXI Graphics
 const button = new PIXI.Graphics();
@@ -43,8 +68,8 @@ buttonText.x = button.x + button.width / 2 - buttonText.width / 2;  // Center th
 buttonText.y = button.y + button.height / 2 - buttonText.height / 2;
 
 // Add button and text to the stage
-app.stage.addChild(button);
-app.stage.addChild(buttonText);
+loaderScene.addChild(button);
+loaderScene.addChild(buttonText);
 
 // Make the button interactive and set the cursor to a pointer
 button.interactive = true;
@@ -63,4 +88,27 @@ function onButtonClick() {
     // You can also perform other actions, like changing the text or triggering other functions
     buttonText.text = 'Clicked!';
 	buttonText.x = button.x + button.width / 2 - buttonText.width / 2;
+
+	app.stage.removeChild(loaderScene);
+	app.stage.addChild(mainScene);
 }
+
+const startGameText = new PIXI.Text('Start Game', style);
+startGameText.x = app.view.width / 2 - startGameText.width / 2;
+startGameText.y = app.view.height / 2 - startGameText.height / 2;
+startGameText.eventMode = "static";
+startGameText.cursor = "pointer";
+
+startGameText.on("click", () => {
+	app.stage.removeChild(loaderScene);
+	app.stage.addChild(mainScene);
+});
+
+loaderScene.addChild(startGameText);
+
+app.stage.addChild(loaderScene);
+
+const mainScene = new PIXI.Container();
+
+
+
